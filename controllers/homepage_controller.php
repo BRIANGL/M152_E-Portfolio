d47f1de996cb4ade7740ee04b5fd72c60a0c9688
BRIANGL <brian.gl@eduge.ch>
2021-02-04 12:55:16 +0100

    chapitre 1

--- controllers/homepage_controller.php
+++ controllers/homepage_controller.php
@@ -177,7 +177,7 @@ function showTemplate()
     <div class="panel panel-default">
         <div class="panel-thumbnail"><img src="assets/img/bg_5.jpg" class="img-responsive"></div>
         <div class="panel-body">
-            <p class="lead">`Txt_Commentaire`</p>
+            <p class="lead">Super Blog</p>
             <p>45 Followers, 13 Posts</p>
             <p><img src="assets/img/uFp_tsTJboUY7kue5XAsGAs28.png" height="28px" width="28px"></p>
         </div>
